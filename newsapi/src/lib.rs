fe4b16dda75fde976dcd06183394b6b09db7253e
Sergey Sivkov <s.sivkov@tionix.ru>
2022-05-15 17:23:06 +0300

    Refactoring newsapi lib

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,5 +1,14 @@
-use std::error::Error;
 use serde::Deserialize;
+
+#[derive(thiserror::Error, Debug)]
+pub enum NewsApiError {
+    #[error("Failed fetching articles")]
+    RequestFailed(ureq::Error),
+    #[error("Failed converting response to string")]
+    FailedResponseToString(std::io::Error),
+    #[error("Article parsing failed")]
+    ArticleParseError(serde_json::Error)
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Articles {
@@ -11,9 +20,10 @@ pub struct Article {
     pub url: String
 }
 
-pub fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
-    let response = ureq::get(url).call()?.into_string()?;
-    let articles = serde_json::from_str::<Articles>(&response)?;
+pub fn get_articles(url: &str) -> Result<Articles, NewsApiError> {
+    let response = ureq::get(url).call().map_err(|e| NewsApiError::RequestFailed(e))
+    ?.into_string().map_err(|e| NewsApiError::FailedResponseToString(e))?;
+    let articles = serde_json::from_str::<Articles>(&response).map_err(|e| NewsApiError::ArticleParseError(e))?;
 
     Ok(articles)
 }
