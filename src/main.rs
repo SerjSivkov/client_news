ea38b21253f749cb71d9efa60aa7159126007b0a
Sergey Sivkov <s.sivkov@tionix.ru>
2022-05-29 17:26:34 +0300

    Move api to struct

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,29 @@ mod theme;
 
 use std::error::Error;
 use dotenv::dotenv;
-use newsapi::{get_articles, Articles};
+use newsapi::{Article, Country, NewsAPI, Endpoint};
+// use newsapi::{get_articles, Articles};
 
-fn render_articles(articles: &Articles) {
+fn render_articles(articles: &Vec<Article>) {
     let theme = theme::default();
     theme.print_text("# Top headlines\n\n");
-    for i in &articles.articles {
-        theme.print_text(&format!("`{}`", i.title));
-        theme.print_text(&format!("> *{}*", i.url));
+    for i in articles {
+        theme.print_text(&format!("`{}`", i.title()));
+        theme.print_text(&format!("> *{}*", i.url()));
         theme.print_text("---");
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
 
     let api_key = std::env::var("API_KEY")?;
-    let url = "https://newsapi.org/v2/top-headlines?country=ru&sortBy=publishedAt&apiKey=";
-    let url = format!("{}{}", url, api_key);
+    let mut newsapi = NewsAPI::new(&api_key);
+    newsapi.endpoint(Endpoint::TopHeadlines).country(Country::Ru);
 
-    let articles = get_articles(&url)?;
+    let newsapi_response = newsapi.fetch()?;
 
-    render_articles(&articles);
+    render_articles(&newsapi_response.articles());
 
     Ok(())
 }
